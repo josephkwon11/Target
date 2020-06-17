60d628e1eb7c21e8a7a9e80fa6d55da24b03cda8
josephkwon11 <67008530+josephkwon11@users.noreply.github.com>
2020-06-17 09:54:35 -0500

    Update Bulk Insert Across Servers.sql

--- Bulk Insert Across Servers.sql
+++ Bulk Insert Across Servers.sql
@@ -1,29 +1,26 @@
---drop table [Target].[dbo].AB_Pricing_Formulas2', 'U')
+IF OBJECT_ID('[Client].[dbo].[NIS_Table]', 'U') IS NOT NULL
+	TRUNCATE TABLE [Client].[dbo].NIS_Table;
 
-IF OBJECT_ID('[Target].[dbo].[AB_Pricing_Formulas2]', 'U') IS NOT NULL
-	TRUNCATE TABLE [Target].[dbo].AB_Pricing_Formulas2;
-
----{DATEFORMAT(MMddyyyy)}
 ELSE
-	CREATE TABLE [Target].[dbo].AB_Pricing_Formulas2(
+	CREATE TABLE [Client].[dbo].NIS_Table(
 	[Market Name] [varchar](50) NULL,
 	[UPC] [bigint] NULL,
 	[UPC Description] [varchar](250) NULL,
 	)
 
-UPDATE STATISTICS [Target].[dbo].[AB_Pricing_Formulas2]
+UPDATE STATISTICS [Client].[dbo].[NIS_Table]
 
 /*****   Bulk Insert  *****/
 
 DECLARE @bulk_cmd varchar(1000)
-SET @bulk_cmd = 'BULK INSERT [Target].[dbo].[AB_Pricing_Formulas2]
-FROM ''\\daywinrcap001\C\Target\NIS.csv''
+SET @bulk_cmd = 'BULK INSERT [Client].[dbo].[NIS_Table]
+FROM ''\\daywinrcap001\H\Client\NIS.csv''
 WITH (DATAFILETYPE = ''char'',
 	  CODEPAGE = ''ACP'',
       FIRSTROW = 2,
       ROWTERMINATOR = ''0x0a'',
-      FIELDTERMINATOR = '','',
+      FIELDTERMINATOR = ''|'',
 	  BATCHSIZE = 10000)'
 EXEC(@bulk_cmd);
 
-UPDATE STATISTICS [Target].[dbo].AB_Pricing_Formulas2
+UPDATE STATISTICS [Client].[dbo].NIS_Table
